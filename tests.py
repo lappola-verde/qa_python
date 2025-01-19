0ba27b295718c536e07a38c12a092f86877e763a
Olga Iudina <t969310824@gmail.com>
2025-01-19 11:20:03 +0000

    First commit to project of 4th sprint

--- tests.py
+++ tests.py
@@ -1,24 +1,70 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.fixture(autouse=True)
+    def collector(self):
         collector = BooksCollector()
+        return collector
 
-        # добавляем две книги
+    books_genre_list = [
+            ['Тайна третьей планеты', 'Фантастика'],
+            ['Дракула', 'Ужасы'],
+            ['Печальный кипарис', 'Детективы'],
+            ['Зверский детектив', 'Мультфильмы'],
+            ['Полный порядок, Дживс!', 'Комедии']
+        ]
+
+    def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    @pytest.mark.parametrize('name, genre', books_genre_list)
+    def test_set_book_genre_set_genre_from_list(self, collector, name, genre):
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.books_genre[name] == genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_book_genre_get_book_genre_for_book_without_genre(self, collector):
+        collector.add_new_book('Дракула')
+        assert collector.get_book_genre('Дракула') == ''
+
+    @pytest.mark.parametrize('name, genre', books_genre_list)
+    def test_get_books_with_specific_genre_get_books_with_genre_from_list(self, collector, name, genre):
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_with_specific_genre(genre) == [name]
+
+    def test_get_books_genre_get_dict_with_one_book(self, collector):
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Дракула', 'Ужасы')
+        assert collector.books_genre == {'Дракула': 'Ужасы'}
+
+    def test_get_books_for_children_get_books_without_genre_age_rating(self, collector):
+        collector.add_new_book('Тайна третьей планеты')
+        collector.set_book_genre('Тайна третьей планеты', 'Фантастика')
+        collector.add_new_book('Дракула')
+        collector.set_book_genre('Дракула', 'Ужасы')
+        assert collector.get_books_for_children() == ['Тайна третьей планеты']
+
+    def test_add_book_in_favorites_add_book_from_dict(self, collector):
+        collector.add_new_book('Тайна третьей планеты')
+        collector.add_book_in_favorites('Тайна третьей планеты')
+        assert collector.favorites == ['Тайна третьей планеты']
+
+    def test_delete_book_from_favorites_delete_only_book_from_favorites(self, collector):
+        collector.add_new_book('Тайна третьей планеты')
+        collector.add_book_in_favorites('Тайна третьей планеты')
+        collector.delete_book_from_favorites('Тайна третьей планеты')
+        assert collector.favorites == []
+
+    def test_get_list_of_favorites_books_get_list_of_added_favorite_books(self, collector):
+        collector.add_new_book('Тайна третьей планеты')
+        collector.add_new_book('Дракула')
+        collector.add_book_in_favorites('Тайна третьей планеты')
+        collector.add_book_in_favorites('Дракула')
+        assert collector.get_list_of_favorites_books() == ['Тайна третьей планеты', 'Дракула']
